cd26e363ab8dd5ab1ffd917d991c102cfad072a7
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-12-25 12:59:47 +0530

    Understanding global mocks

--- src/no-framework/external-mock-module.js
+++ src/no-framework/external-mock-module.js
@@ -6,36 +6,33 @@
 
 function fn(impl = () => {}) {
   const mockFn = (...args) => {
-    mockFn.mock.calls.push(args)
-    return impl(...args)
-  }
-  mockFn.mock = {calls: []}
-  return mockFn
+    mockFn.mock.calls.push(args);
+    return impl(...args);
+  };
+  mockFn.mock = { calls: [] };
+  return mockFn;
 }
 
-const utilsPath = require.resolve('../utils')
-require.cache[utilsPath] = {
-  id: utilsPath,
-  filename: utilsPath,
-  loaded: true,
-  exports: {
-    getWinner: fn((p1, p2) => p1),
-  },
-}
+require('../__no-framework-mocks__/utils');
 
-const assert = require('assert')
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
+const mockedUtilsPath = require.resolve('../__no-framework-mocks__/utils');
+const utilsPath = require.resolve('../utils');
 
-const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-assert.strictEqual(winner, 'Kent C. Dodds')
+require.cache[utilsPath] = require.cache(mockedUtilsPath);
+
+const assert = require('assert');
+const thumbWar = require('../thumb-war');
+const utils = require('../utils');
+
+const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler');
+assert.strictEqual(winner, 'Kent C. Dodds');
 assert.deepStrictEqual(utils.getWinner.mock.calls, [
   ['Kent C. Dodds', 'Ken Wheeler'],
   ['Kent C. Dodds', 'Ken Wheeler'],
-])
+]);
 
 // cleanup
-delete require.cache[utilsPath]
+delete require.cache[utilsPath];
 
 /**
  * Checkout master branch to see the answer.
